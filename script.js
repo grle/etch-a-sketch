4ba48ca71f0676a768af67bcfca7a4d918677426
Grace Lee <graceleenexus@gmail.com>
2022-08-11 13:07:52 -0700

    update features

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ function myGrid(num) {
   //row
   for (let i = 0; i < dimension; i++) {
     let row = document.createElement("div");
-    row.addEventListener('mouseover', (event) => {
+    row.style.backgroundColor = "#FFFFFF";
+    row.addEventListener("mouseover", (event) => {
       draw(row);
     });
-    container.appendChild(row).className = "grid-item base";
+    container.appendChild(row).className = "grid-item";
   }
   //column
   container.style.setProperty("grid-template-columns", `repeat(${num},1fr)`);
@@ -37,19 +38,88 @@ function clearGrid() {
 myGrid(slider.value);
 
 //DRAWING
+let boolBrush = true;
+let boolEraser = false;
+let boolShade = false;
+let boolPsyche = false;
+let boolColorPick = false;
+
 function draw(cell) {
-  cell.classList.add("hold");
+  if (boolBrush == true) {
+    cell.style.backgroundColor = "#808080";
+  }
+  else if (boolEraser == true) {
+    cell.style.backgroundColor = "#FFFFFF";
+  }
+  // else if (boolShade == true) {
+  // }
+  else if (boolPsyche == true) {
+    let psycheHex = pastelHex();
+    cell.style.backgroundColor = psycheHex;
+  }
+  else {
+    console.log("error");
+  }
+}
+
+function mainBrush() {
+  boolBrush = true;
+  boolPsyche = false;
+  boolShade = false;
+  boolEraser = false;
+}
+
+function psychedelic() {
+  boolBrush = false;
+  boolPsyche = true;
+  boolShade = false;
+  boolEraser = false;
+}
+
+function shading() {
+  boolBrush = false;
+  boolPsyche = false;
+  boolShade = true;
+  boolEraser = false;
 }
 
 function eraser() {
+  boolBrush = false;
+  boolPsyche = false;
+  boolShade = false;
+  boolEraser = true;
+}
 
+function pastelHex() {
+  let num = Math.floor(Math.random() * 6); //generate number 0-5
+  if (num == 0) {
+    return "#FFF9CA"; //yellow
+  }
+  if (num == 1) {
+    return "#FFDEB4"; //orange
+  }
+  if (num == 2) {
+    return "#FFB4B4"; //red
+  }
+  if (num == 3) {
+    return "#B2A4FF"; //purple
+  }
+  if (num == 4) {
+    return "#C1EFFF"; //blue
+  }
+  if (num == 5) {
+    return "#CFE8A9"; //green
+  }
+}
+
+function darkenTen(rgb) {
+  //fill this in later
 }
 
 function clearMe() {
   const total = document.querySelectorAll(".grid-item");
   total.forEach(item => {
-    item.classList.remove("hold");
-    item.classList.add("base");
+    item.style.backgroundColor = "white";
   });
 }
 
